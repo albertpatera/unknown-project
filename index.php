3bc14358974883fd38b9353b47b899de0c849b9d
Albert Patera <=>
2019-07-05 20:30:53 +0000

    first commit file

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+	<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 mx-auto">
+			<div class="mx-auto">
+			<div class="card col-md-6 p-5 m-5">
+  				<div class="card-heading text-primary text-justify">
+					<h1 class="display-4">IT Network project</h1>
+					<h2 class="display-6">Unknown web application</h2>
+					<hr>	
+				</div>
+				<div class="card-body text-justify">
+					<p class="text-primary">Error, page not found....really shame</p>
+				</div>
+				<hr>
+				<div class="row">
+			
+				<div class="col-md-4">
+					<h3 class="display-6 text-secondary">
+						CHANGELOG
+					</h3>	
+				</div>
+					<div class="col-md-8">
+						<p class="text-muted">
+							GIT repository created. Link s <a href="#">here</h1>
+						</p>	
+					</div>
+  					</div>
+					<hr>
+					<div class="text muted text-justify">
+    						<a href="mailto:info@albertpatera.cz">Albert Patera</a>
+						<a href="list-item text-center">GitHub</a>
+  					</div>
+					<hr>
+						<div class="text-muted text-center">
+							<?php
+								echo $_SERVER["HTTP_HOST"];   
+								echo $_SERVER["REMOTE_ADDR"];
+								echo $_SERVER["SERVER_ADDR"];
+								echo $_SERVER["SERVER_SOFTWARE"];
+								echo $_SERVER["DOCUMENT_ROOT"];
+
+								 
+							?>	
+						</div>
+						<h4 class="display-6 text-center">
+							<?php
+								echo $_SERVER["DOCUMENT_ROOT"];
+
+								 
+							?>	
+						</h4>
+					</div>
+</div>
+		</div>
+	</div>
+</div>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
